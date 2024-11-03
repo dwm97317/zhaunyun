2e60c705ead66495c54d22bfcefe202ead670802
冯小狼 <1835504221@qq.com>
2024-11-03 21:56:21 +0800

    g

--- source/application/store/service/Auth.php
+++ source/application/store/service/Auth.php
@@ -69,7 +69,8 @@ class Auth
         'tools/search',
         'tr_order/zddeliverysave',
         'package.index/importdo',
-        'package.newpack/newadd'
+        'package.newpack/newadd',
+        'package.newpack/newsavepackage'
     ];
 
     /** @var array $accessUrls 商家用户权限url */
