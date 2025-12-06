c0213e8d916fb1224a374b7583e2bdcc2aeb9c38
冯小狼 <1835504221@qq.com>
2025-12-06 18:27:08 +0800

    优化

--- source/application/common.php
+++ source/application/common.php
@@ -1401,7 +1401,7 @@ function send_mail($tomail, $name, $subject = '', $body = '', $attachment = null
     $mail = new Email();         //实例化PHPMailer对象
     $mail->CharSet = 'UTF-8';           //设定邮件编码，默认ISO-8859-1，如果发中文此项必须设置，否则乱码
     $mail->IsSMTP();                    // 设定使用SMTP服务
-    $mail->SMTPDebug = 1;               // SMTP调试功能 0=关闭 1 = 错误和消息 2 = 消息
+    $mail->SMTPDebug = 0;               // SMTP调试功能 0=关闭 1 = 错误和消息 2 = 消息
     $mail->SMTPAuth = true;             // 启用 SMTP 验证功能
     $mail->SMTPSecure = 'ssl';          // 使用安全协议
     $mail->Host = "smtp.qq.com"; // SMTP 服务器
